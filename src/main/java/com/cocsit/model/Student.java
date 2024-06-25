c702a7c1a0c84be3dbebe3f25dc576d1266cabf0
shubham <shiketodshubham@gmail.com>
2024-06-25 20:52:46 +0530

    Initial Coomit

---
+++ src/main/java/com/cocsit/model/Student.java
@@ -0,0 +1,16 @@
+package com.cocsit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Student_TBL")
+public class Student {
+	
+	
+	
+	private Integer sid;
+	private String studentName;
+	private 
+
+}
